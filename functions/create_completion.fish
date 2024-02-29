6de96af923c47e4eebb2746d8a6110de940b0dc7
Dionysis Zindros <dionyziz@gmail.com>
2024-02-29 21:44:37 +0100

    Do not pass suggested completions to commandline command

--- functions/create_completion.fish
+++ functions/create_completion.fish
@@ -5,6 +5,6 @@ function create_completion
     set buffer (commandline -b)
     set cursor_pos (commandline -C)
     set completion (echo -n "$buffer" | $dir/create_completion.py $cursor_pos)
-    commandline -i $completion
+    commandline -i -- $completion
 end
 
